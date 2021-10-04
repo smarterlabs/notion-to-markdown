ed48560ed11ad5284f073106f8b5ce961d672388
Kennedy Rose <kennedyianrose@gmail.com>
2021-10-03 20:38:51 -0500

    Docusaurus

--- packages/notion-to-markdown/src/index.js
+++ packages/notion-to-markdown/src/index.js
@@ -4,7 +4,9 @@ import potion from '../../potion/api/html.js'
 	const res = await potion({
 		query: {
 			// https://smarterlabs.notion.site/Setting-Up-a-New-Vercel-Website-584a1c27a10642d8869473588a5c1b45
-			id: `584a1c27a10642d8869473588a5c1b45`,
+			// id: `584a1c27a10642d8869473588a5c1b45`,
+			// https://www.notion.so/asnunes/Simple-Page-Text-4d64bbc0634d4758befa85c5a3a6c22f
+			id: `4d64bbc0634d4758befa85c5a3a6c22f`,
 		}
 	}, {
 		json: obj => console.log(`obj`, obj),
